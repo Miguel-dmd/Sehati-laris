30478b73850d4a301b1b99191d1df9aad6131f99
Migwuel <darkness8577@gmail.com>
2026-06-30 22:42:25 +0700

    edit tailwind

--- sehati-laris/tailwind.config.js
+++ sehati-laris/tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#8b6b4a',
+        'primary-dark': '#6c5644',
+        'accent': '#a9914a',
+        'cream': '#e9d8ae',
+        'text-dark': '#77614f',
+        'bg-light': '#f5f5f5',
+      },
+      fontFamily: {
+        sans: ['Inter', 'Arial', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
-
